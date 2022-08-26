1a1eb68b33d1951eb7bd2c1b2b2d74ea8d0fa8d0
unknown <Arturo@CL050DQBX9ADFW.cfe.mx>
2022-08-26 16:47:54 -0500

    Last Version, Adjustment in estimate of worked hours.

--- src/acme.php
+++ src/acme.php
@@ -13,24 +13,18 @@ if ($handle) {
 function validateLine($a)
 {
 	$wordsize=strlen(chop($a));
-	$name="xxxxx";
 	$namesize=0;
-    //echo nl2br($a);
+
     for($i=0;$i<=$wordsize;$i++)
     {
-    	//echo "i=".$i;
-    	//echo "wordsize:".$wordsize;
     	$cad=substr($a,$i,1);
-    	//echo "cad=".$cad;
     	if($cad=='='){
     		$name=substr($a, 0,$i);
     		$namesize=$i;
-    		//echo $name;
     	}
     }
     if($namesize>0)
     {
-       	echo nl2br($name);
         validateHours($name,$a,$wordsize,$namesize);
     }
 }
@@ -43,26 +37,19 @@ function validateHours($nm,$cl,$ws,$ns)
 	$workeddays=($ws-$ns)/$ranksize;
 	$amount=0;
 	$ns++;
-	echo "workeddays=".$workeddays;
-	echo "ws = ".$ws;
-	echo "ns = ".$ns;
-	echo " primerdia=".substr($cl, $ns,$ranksize);
 	for($j=$workeddays;$j>0;$j--)
 	{
-		//Días
-		//echo "dd=".substr($cl, $ns,$daysize);
+		//Days
 		$day=substr($cl, $ns,$daysize);
 		$inhr=substr($cl, ($ns+$daysize),$hrsize);
 		$outhr=substr($cl, ($ns+$daysize+$hrsize+1),$hrsize);
-		echo "<br>"."dd=".$day;
 		$ns=$ns+$ranksize;
 
-		//Horas
-		$datex7 = new DateTime($outhr);
-		$datex77 = new DateTime($inhr);
-		$horax7 = date_diff($datex7, $datex77);
-		echo "<br>";
-		echo $workedhrs = $horax7->format('%H');
+		//Hours
+		$hr2 = new DateTime($outhr);
+		$hr1 = new DateTime($inhr);
+		$difhr = date_diff($hr2, $hr1);
+		$workedhrs = $difhr->format('%H');
 
 		switch ($day) {
 			case 'MO':
@@ -70,42 +57,32 @@ function validateHours($nm,$cl,$ws,$ns)
 			case 'WE':
 			case 'TH':
 			case 'FR':
-				echo " entresemana entró a las ".$inhr;
-				echo " y salió a las ".$outhr;
-				echo " trabajó ".$workedhrs." hrs";
 				if ($inhr>"00:00" && $inhr<"09:00"){
 					$salary=25;
-				}elseif ($inhr>"09:00" && $inhr<"18:00") {
+				}elseif ($inhr>="09:00" && $inhr<"18:00") {
 					$salary=15;
 				}else{
 					$salary=20;
 				}
-				/*00:01 - 09:00 25 USD
-				09:01 - 18:00 15 USD
-				18:01 - 00:00 20 USD*/
 				break;
 			case 'SA':
 			case 'SU':
-				echo " finecito entró a las ".$inhr;
-				echo " y salió a las ".$outhr;
-				echo " trabajó ".$workedhrs." hrs";
 				if ($inhr>"00:00" && $inhr<"09:00"){
 					$salary=30;
-				}elseif ($inhr>"09:00" && $inhr<"18:00") {
+				}elseif ($inhr>="09:00" && $inhr<"18:00") {
 					$salary=20;
 				}else{
 					$salary=25;
 				}
 				break;	
 			default:
-				echo "no existe dia";
+				echo "Error. Please check the days abbreviations.";
 				break;
 		}
 		$pay=$workedhrs*$salary;
 		$amount=$amount+$pay;
-		echo "El $day cobró $ $pay por $workedhrs trabajadas.";
 	}
-	echo "<br>The amount to pay ".$nm." is : ".$amount." USD.<br>";    
+	echo "The amount to pay ".$nm." is : ".$amount." USD.<br>";    
 }
 
 ?>
